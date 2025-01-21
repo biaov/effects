bc413137c0ca033956cae367a4048b210e60e971
biaov <biaov@qq.com>
2025-01-21 09:20:11 +0800

    更新首页

--- home/src/components/view-list/index.vue
+++ home/src/components/view-list/index.vue
@@ -12,7 +12,8 @@ const { listData, onNavTo } = useViewList()
   <div class="wrap">
     <view class="header">
       <div class="title">特效集锦</div>
-      <a target="_blank" href="https://github.com/biaov/effects" title="访问源码"><img src="@/assets/github.svg" alt="github" class="github" /></a>
+      <a target="_blank" href="https://github.com/biaov/effects" title="访问源码"><img src="@/assets/github.svg"
+          alt="github" class="github" /></a>
     </view>
     <div class="container">
       <div v-for="(item, index) in listData" :key="index" class="item" @click="onNavTo(item)">
@@ -36,24 +37,30 @@ const { listData, onNavTo } = useViewList()
   from {
     transform: translateY(0px);
   }
+
   to {
     transform: translateY(-15px);
   }
 }
+
 .wrap {
   position: relative;
   z-index: 2;
-  width: 1200px;
-  padding: 40px 0;
+  max-width: 1240px;
+  width: 100%;
+  padding: 20px;
   margin: 0 auto;
+
   .header {
     display: flex;
     justify-content: space-between;
     padding: 40px 0;
+
     .title {
       font-size: 30px;
       font-weight: bold;
     }
+
     .github {
       width: 40px;
       height: 40px;
@@ -69,55 +76,75 @@ const { listData, onNavTo } = useViewList()
       }
     }
   }
+
   .container {
     display: flex;
     flex-wrap: wrap;
-    width: calc(100% + 20px);
+    width: 100%;
+    gap: 20px;
+
     .item {
       width: 386px;
-      margin: 0 20px 20px 0;
+      margin-bottom: 20px;
       border-radius: 10px;
       box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
       background: #fff;
       transition: all 0.3s ease;
       animation: floating ease-in-out 1.6s infinite alternate;
       cursor: pointer;
+
       &:hover {
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
       }
+
       .img {
         height: 300px;
         border-radius: 10px 10px 0 0;
         overflow: hidden;
       }
+
       .text {
         padding: 30px;
         color: rgba(0, 0, 0, 0.45);
         font-size: 14px;
+
         .title {
           margin-bottom: 10px;
           color: rgba(0, 0, 0, 0.85);
           font-weight: bold;
           font-size: 16px;
         }
+
         .stack {
           color: #fff;
-          margin-bottom: 10px;
           display: inline-flex;
           flex-wrap: wrap;
+          gap: 10px;
+
           span {
-            margin-right: 10px;
+            margin-bottom: 10px;
             padding: 2px 8px;
             border-radius: 4px;
             background: #f0f0f0;
             @bgcs: #1677ff, #eb2f96, #13c2c2, #722ed1, #52c41a, #fa8c16;
-            each(@bgcs,{
-              &:nth-child(@{index}){
+
+            each(@bgcs, {
+              &:nth-child(@{index}) {
                 background: @value;
               }
             });
-          }
         }
+      }
+    }
+  }
+}
+}
+
+@media (max-width:800px) {
+  .wrap {
+    .container {
+      .item {
+        width: 100%;
       }
     }
   }
