19bb279344b5b3ca7a0b27e51beeb5df86ebc885
biaov <biaov@qq.com>
2025-10-30 13:57:37 +0800

    繁简体转换器

--- simplified-traditional/js/index.js
+++ simplified-traditional/js/index.js
@@ -1,5 +1,38 @@
-import { simplifiedToTraditional, traditionalToSimplified } from './utils.js'
+import { simplifiedTransformTraditional } from './utils.js'
+
+const message = (value, type, el) => {
+  el.classList.remove('error', 'success')
+  type && el.classList.add(type)
+  el.textContent = value
+}
 
 window.addEventListener('load', () => {
   const textarea = document.querySelector('.textarea')
+  const buttons = document.querySelector('.buttons')
+  const tips = document.querySelector('.tips')
+
+  buttons.addEventListener('click', e => {
+    if (!e.target.classList.contains('btn')) return
+    const value = textarea.value
+    if (!value) {
+      message('请输入内容', 'error', tips)
+      return
+    }
+    const { type } = e.target.dataset
+    switch (type) {
+      case 'simplified':
+      case 'traditional': {
+        const newValue = simplifiedTransformTraditional(value, type === 'traditional')
+        textarea.value = newValue
+        return
+      }
+      case 'clear':
+        textarea.value = ''
+        break
+      case 'copy':
+        navigator.clipboard.writeText(textarea.value).then(message.bind(null, '复制成功', 'success', tips)).catch(message.bind(null, '复制失败', 'error', tips))
+        return
+    }
+    message('', '', tips)
+  })
 })
--- simplified-traditional/js/utils.js
+++ simplified-traditional/js/utils.js
@@ -1,36 +1,44 @@
-import textData from './text-data'
+import textData from './text-data.js'
 
-const checkChinese = value => /[19968-40869]+/.test(value.charCodeAt())
+/**
+ * 检查是否为中文字符
+ */
+const checkChinese = value => {
+  const code = value.charCodeAt()
+  if (!code) return false
+  return code >= 19968 && code <= 40869
+}
 
 /**
  * 简体转繁体
  */
-export const simplifiedToTraditional = value => {
-  if (!value) return value
-  value
-    .split('')
-    .map(value => {
-      if (!checkChinese(value)) return value
-      const item = textData.find(([simplified]) => simplified === value)
-      if (!item) return value
-      const traditional = item[1]
-      return Array.isArray(traditional) ? traditional[0] : traditional
-    })
-    .join('')
+const simplifiedToTraditional = value => {
+  if (!checkChinese(value)) return value
+  const item = textData.find(([simplified]) => simplified === value)
+  if (!item) return value
+  const traditional = item[1]
+  return Array.isArray(traditional) ? traditional[0] : traditional
 }
 
 /**
  * 繁体转简体
  */
-export const traditionalToSimplified = value => {
+const traditionalToSimplified = value => {
+  if (!checkChinese(value)) return value
+  const item = textData.find(([_, traditional]) => (Array.isArray(traditional) ? traditional.includes(value) : traditional === value))
+  if (!item) return value
+  return item[0]
+}
+
+/**
+ * 简体=>繁体
+ * 简体<=繁体
+ * to: simplified | traditional
+ */
+export const simplifiedTransformTraditional = (value, toTraditional = false) => {
   if (!value) return value
-  value
+  return value
     .split('')
-    .map(value => {
-      if (!checkChinese(value)) return value
-      const item = textData.find(([_, traditional]) => (Array.isArray(traditional) ? traditional.includes(value) : traditional === value))
-      if (!item) return value
-      return item[0]
-    })
+    .map(toTraditional ? simplifiedToTraditional : traditionalToSimplified)
     .join('')
 }
